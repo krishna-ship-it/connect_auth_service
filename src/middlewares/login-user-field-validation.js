e8782c30c7c495342df56a5fce8fc42e4f8ee860
Krishna Pandey <krish36pandey@gmail.com>
2023-12-20 12:21:07 +0530

    added custom error class for error handling

--- src/middlewares/login-user-field-validation.js
+++ src/middlewares/login-user-field-validation.js
@@ -1,14 +1,17 @@
+const ApiError = require("../utils/errors/ApiError");
+
 const loginValidation = (req, res, next) => {
-  console.log(req.body);
   if (
     !req.body ||
     !req.body.email ||
     !req.body.email.includes("@") ||
     !req.body.email.includes(".")
   )
-    return next(new Error("please enter a valid email"));
-  if (!req.body.password) return next(new Error("password is required"));
-  console.log("login validator end");
+    return next(
+      new ApiError("please enter a valid email", 400, "validationError")
+    );
+  if (!req.body.password)
+    return next(new ApiError("password is required", 400, "validationError"));
   next();
 };
 
