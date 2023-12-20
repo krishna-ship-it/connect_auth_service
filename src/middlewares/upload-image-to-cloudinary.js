9e74c9c33b3a21bdab9b401e767daa0ffb9aa476
Krishna Pandey <krish36pandey@gmail.com>
2023-12-20 12:19:42 +0530

    added a extra check to check request contains avatar or not

--- src/middlewares/upload-image-to-cloudinary.js
+++ src/middlewares/upload-image-to-cloudinary.js
@@ -1,6 +1,9 @@
 const { v2: cloudinary } = require("cloudinary");
+const ApiError = require("../utils/errors/ApiError");
 const imageUploader = async (req, res, next) => {
   try {
+    if (!req.files || !req.files.avatar)
+      return next(new ApiError("image is required", 400, "BadRequest"));
     const uploadedImage = await cloudinary.uploader.upload(
       req.files.avatar.tempFilePath
     );
@@ -8,7 +11,13 @@ const imageUploader = async (req, res, next) => {
     req.body.avatar_public_url = uploadedImage.secure_url;
     next();
   } catch (err) {
-    return next(new Error("failed to upload image , please try again letter"));
+    return next(
+      new ApiError(
+        `failed to upload image , please try again letter,${err.message}`,
+        500,
+        "serverError"
+      )
+    );
   }
 };
 
