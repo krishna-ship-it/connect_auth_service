098c807c194d682c8df8c4aee7119da84e70a503
Krishna Pandey <krish36pandey@gmail.com>
2023-12-20 12:16:01 +0530

    added update profile picture service

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const { UserRepository } = require("./../repositories/index");
 const bcrypt = require("bcrypt");
 const { signJwt } = require("./../utils/commons/jwt");
+const ApiError = require("../utils/errors/ApiError");
 class UserService {
   static async signup(userData) {
     try {
@@ -20,14 +21,32 @@ class UserService {
   }
   static async login(email, password) {
     try {
-      const user = await UserRepository.login(email, password);
+      const user = await UserRepository.login(email);
       if (await bcrypt.compare(password, user.password)) {
         delete user.dataValues.password;
         const token = await signJwt({ id: user.id });
         return { user, token };
-      } else throw new Error("invalid credentials");
+      } else throw new ApiError("invalid credentials", 401, "serverError");
     } catch (err) {
       throw err;
+    }
+  }
+  static async updateProfilePicture(
+    user_id,
+    avatar_public_id,
+    avatar_public_url
+  ) {
+    try {
+      const user = await UserRepository.findUserById(user_id);
+      user.avatar_public_id = avatar_public_id;
+      user.avatar_public_url = avatar_public_url;
+      return UserRepository.updateUserProfile(user);
+    } catch (err) {
+      throw new ApiError(
+        "internal server error while updating the profile picture (service Layer)",
+        500,
+        "serverError"
+      );
     }
   }
 }
