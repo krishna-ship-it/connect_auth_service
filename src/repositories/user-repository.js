7693fd3bcb7634d4cb0517897b43310799fbc54a
Krishna Pandey <krish36pandey@gmail.com>
2023-12-20 12:17:58 +0530

    added find user by id a & update profile picture method

--- src/repositories/user-repository.js
+++ src/repositories/user-repository.js
@@ -1,3 +1,4 @@
+const ApiError = require("../utils/errors/ApiError");
 const { User } = require("./../models/index");
 class UserRepository {
   static async signup(userData) {
@@ -6,7 +7,11 @@ class UserRepository {
       delete user.dataValues.password;
       return user;
     } catch (err) {
-      throw err;
+      throw new ApiError(
+        "internal server error while signing up(Repository Layer)",
+        500,
+        "serverError"
+      );
     }
   }
   static async delete(id) {
@@ -17,18 +22,57 @@ class UserRepository {
         },
       });
     } catch (err) {
-      throw err;
+      throw new ApiError(
+        "internal server error(Repository Layer)",
+        500,
+        "serverError"
+      );
     }
   }
-  static async login(email, password) {
+  static async login(email) {
     try {
       const user = await User.findOne({
         where: { email },
       });
-      if (!user) throw new Error("invalid credentials");
+      if (!user) throw new ApiError("invalid credentials", 401, "serverError");
       return user;
     } catch (err) {
-      throw err;
+      throw new ApiError(
+        "internal server error while logging in (Repository Layer)",
+        500,
+        "serverError"
+      );
+    }
+  }
+  static async findUserById(id) {
+    try {
+      const user = await User.findOne({
+        where: {
+          id,
+        },
+      });
+      if (!user) throw new ApiError("user does not exists", 400, "BadRequest");
+      return user;
+    } catch (error) {
+      if (error.name === "BadRequest") throw error;
+      throw new ApiError(
+        "internal server error while searching the user",
+        500,
+        "serverError"
+      );
+    }
+  }
+  static async updateUserProfile(userInstance) {
+    try {
+      await userInstance.save();
+      delete userInstance.dataValues.password;
+      return userInstance;
+    } catch (err) {
+      throw new ApiError(
+        "internal server error while updating the profile picture",
+        500,
+        "serverError"
+      );
     }
   }
 }
